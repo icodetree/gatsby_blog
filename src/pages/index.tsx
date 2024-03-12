1cdf60534735d22a523d7df1b6a8ba772851e242
icodetree <icodetree@naver.com>
2024-03-12 22:07:52 +0900

    16번에러 건너뜀..

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -74,7 +74,7 @@ export const getPostList = graphql`
             categories
             thumbnail {
               childImageSharp {
-                gatsbyImageData(width: 768, height: 400)
+                gatsbyImageData(width : 768, height:400)
               }
             }
           }
